98e1812e6b72a9e99a1b8ebb3b54f790c605bd7d
Vaibhav Deoda <vaibhavdeoda@gmail.com>
2018-08-16 11:06:48 +0530

    added navigation bar with categories

---
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["Category"];
+
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+
+    }
+}
--- SportsStore/SportsStore/Controllers/ProductController.cs
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -23,7 +23,10 @@ namespace SportsStore.Controllers
             {
                 CurrentPage = productPage,
                 ItemsPerPage = pageSize,
-                TotalItems = repository.Products.Count()
+                TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e =>
+                    e.Category == category).Count()
             },
             CurrentCategory = category
         });
